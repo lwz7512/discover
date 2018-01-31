6319ae5c15a97e9063baf4d3733a580a2b25a5f2
lwz7512 <lwz7512@gmail.com>
2018-01-31 09:05:43 +0800

    fix button lost bug...

--- src/pages/complete/complete.js
+++ src/pages/complete/complete.js
@@ -1,5 +1,6 @@
 // 获取全局应用程序实例对象
 const app = getApp()
+const Promise = require('../../utils/bluebird')
 
 // 创建页面实例对象
 Page({
@@ -8,13 +9,123 @@ Page({
    */
   data: {
     title: 'complete',
-    btnSelected: false,
+    btnSelected: false, // FIXME, ...
     options: [
       '../../images/opt-bg-0.jpg','../../images/opt-bg-1.jpg',
       '../../images/opt-bg-2.jpg','../../images/opt-bg-3.jpg'
     ],
     background: null,
-    canvas: 'thirdCanvas'
+    canvas: 'thirdCanvas',
+    editMode: true // default is edit mode, false while sharing
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   * 注：这里不适合密集计算!
+   * @2018/01/30
+   */
+  onLoad () {
+    var system = wx.getSystemInfoSync()
+    // console.log(system)
+
+    var screenWidth = system.windowWidth
+    var screenHeight= system.windowHeight
+
+    this.setData({
+      circleRect: {},// toggle switch button, init in later drawing function
+      btnImgs: [
+        { // background 0
+          x: screenWidth - 60,
+          y: screenHeight * 0.24,
+          right: screenWidth,
+          bottom: screenHeight * 0.24 + 40 // width, height = 40
+        },{ // background 1
+          x: screenWidth - 60,
+          y: screenHeight * 0.24 + 50,
+          right: screenWidth,
+          bottom: screenHeight * 0.24 + 50 + 40 // width, height = 40
+        },{ // background 2
+          x: screenWidth - 60,
+          y: screenHeight * 0.24 + 100,
+          right: screenWidth,
+          bottom: screenHeight * 0.24 + 100 + 40 // width, height = 40
+        },{ // background 3
+          x: screenWidth - 60,
+          y: screenHeight * 0.24 + 150,
+          right: screenWidth,
+          bottom: screenHeight * 0.24 + 150 + 40 // width, height = 40
+        }
+      ],
+      btnRoundRect: { // share button
+        x: 40,
+        y: screenHeight - 116,
+        right: 120,
+        bottom: screenHeight - 90
+      }
+    })
+  },
+
+  // TODO: 检测是否点击了按钮
+  canvasTap (e) {
+    // console.log(e);
+    var clientX = e.touches[0].clientX
+    var clientY = e.touches[0].clientY
+
+    var touchedSwitchBtn = this.detectInRect(clientX, clientY, this.data.circleRect)
+    // 重汇此按钮和背景选择按钮
+    if(touchedSwitchBtn){
+      var previous = this.data.btnSelected
+      this.setData({btnSelected:!previous}) // reverse operation
+      this.safeDrawScreen()
+    }
+
+    var touchedSwitchOpt0 = this.detectInRect(clientX, clientY, this.data.btnImgs[0])
+    // 切换背景图绘制 - 0
+    if(touchedSwitchOpt0){
+      // console.log('image 0 touched!');
+      var image = this.data.options[0]
+      this.setData({background: image})
+      this.safeDrawScreen()
+    }
+    var touchedSwitchOpt1 = this.detectInRect(clientX, clientY, this.data.btnImgs[1])
+    // 切换背景图绘制 - 1
+    if(touchedSwitchOpt1){
+      // console.log('image 1 touched!');
+      var image = this.data.options[1]
+      this.setData({background: image})
+      this.safeDrawScreen()
+    }
+    var touchedSwitchOpt2 = this.detectInRect(clientX, clientY, this.data.btnImgs[2])
+    // 切换背景图绘制 - 2
+    if(touchedSwitchOpt2){
+      // console.log('image 2 touched!');
+      var image = this.data.options[2]
+      this.setData({background: image})
+      this.safeDrawScreen()
+    }
+    var touchedSwitchOpt3 = this.detectInRect(clientX, clientY, this.data.btnImgs[3])
+    // 切换背景图绘制 - 3
+    if(touchedSwitchOpt3){
+      // console.log('image 3 touched!');
+      var image = this.data.options[3]
+      this.setData({background: image})
+      this.safeDrawScreen()
+    }
+
+    // TODO, 检测是否点下了分享按钮, 改变模式
+    var touchedShareBtn = this.detectInRect(clientX, clientY, this.data.btnRoundRect)
+    if(touchedShareBtn){
+      // console.log('share btn touched!')
+      this.setData({editMode: false})
+      this.safeDrawScreen().then(()=>{
+        this.createImage()
+      })
+    }
+  },
+
+  detectInRect (x, y, rect) {
+    if(x > rect.x && y > rect.y && x < rect.right && y < rect.bottom) return true
+    return false
   },
 
   // compose image
@@ -39,7 +150,9 @@ Page({
           filePath: res.tempFilePath,
           success: (res) => {
             that.showToast('请在相册中查看生成的图片', 'success')
-            console.log("success:" + res);
+            // restore edit mode
+            that.setData({editMode: true})
+            that.safeDrawScreen()
           }, fail(e) {
             console.log("err:" + e);
           }
@@ -57,37 +170,6 @@ Page({
     });
   },
 
-  changeBG (evt) {
-    // console.log(evt)
-    var i = Number(evt.currentTarget.dataset.index)
-    var image = this.data.options[i]
-    // console.log(image);
-    this.setData({background: image})
-    // redraw
-    wx.getImageInfo({
-      src: this.data.background,
-      success: res => {
-        // console.log(res);
-        this.drawAll(this.data.background, res.width, res.height)
-      }
-    })
-  },
-
-  switchImgs () {
-    var last = this.data.btnSelected
-    this.setData({btnSelected: !last})
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   * 注：这里不适合密集计算!
-   * @2018/01/30
-   */
-  onLoad () {
-    // console.log(app.data);
-  },
-
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -95,13 +177,9 @@ Page({
     // console.log(new Date().getTime());
     var defaultBG = this.data.options[0]
     this.setData({background: defaultBG})
-    // 必须得到图片的大小以计算缩放比例，不然没法画
-    wx.getImageInfo({
-      src: this.data.background,
-      success: res => {
-        // console.log(res);
-        this.drawAll(this.data.background, res.width, res.height)
-      }
+
+    this.safeDrawScreen().then(()=>{
+      console.log('image rendered!');
     })
   },
 
@@ -109,6 +187,23 @@ Page({
     context.setFillStyle('#FFFFFF')
     context.setFontSize(fontSize)
     context.fillText(text, x, y)
+  },
+
+  // 拿到背景图片信息后绘制
+  safeDrawScreen () {
+    return new Promise((resolve, reject) => {
+      // 必须得到背景图片的大小以计算缩放比例，不然没法画
+      wx.getImageInfo({
+        src: this.data.background,
+        success: res => {
+          // console.log(res);
+          this.drawAll(this.data.background, res.width, res.height)
+          resolve()
+        },
+        fail: reject
+      })
+    })
+
   },
 
   drawAll (imgPath, imgWidth, imgHeight) {
@@ -124,9 +219,9 @@ Page({
     context.restore()
 
     var startX = screenWidth/3
-    var startY = 120
+    var startY = screenHeight/6
     var rectW = screenWidth/3
-    var rectH = screenHeight*2.2/5
+    var rectH = screenHeight*2.24/5
 
     // draw white rectangle
     context.setLineWidth(2);
@@ -156,15 +251,15 @@ Page({
     }
 
     // draw kpmg wish for new year
-    var wish = '毕马威祝您在新的一年里能够发现机遇，'
-    var wish2 = '实现梦想'
-    this.drawLargeText(context, 18, wish, 40, screenHeight-180)
-    this.drawLargeText(context, 18, wish2, 40, screenHeight-140)
+    var wish1 = '发现机遇，实现梦想'
+    var wish2 = '毕马威祝您在新的一年里心想事成'
+    this.drawLargeText(context, 18, wish1, 40, screenHeight*0.7)
+    this.drawLargeText(context, 18, wish2, 40, screenHeight*0.7+30)
 
     // draw qrcode image
     context.save()
     context.scale(0.6, 0.6)
-    context.drawImage('../../images/discoverlogo.jpg', screenWidth*1.2, screenHeight*1.26+20)
+    context.drawImage('../../images/discoverlogo.jpg', screenWidth*1.2, screenHeight*1.3)
     context.restore()
 
     // draw kpmg logo
@@ -173,7 +268,76 @@ Page({
     context.drawImage('../../images/kpmg-200.png', 60, 60)
     context.restore()
 
+    // draw round rectangle button
+    if(this.data.editMode){
+      this.drawRoundRect(context, this.data.btnRoundRect, 14, '#DE4323')
+      // draw background switch image
+      var cntrX = screenWidth - 40
+      var cntrY = screenHeight * 0.18
+      var r = 22
+      var imgPath = this.data.btnSelected?'../../images/img-dn-icon.png':'../../images/img-up-icon.png'
+      this.drawCircleImg(context, cntrX, cntrY, r, imgPath)
+
+      // TODO,  draw background options
+      if(this.data.btnSelected){
+        context.drawImage('../../images/icn-bg-0-40.png', this.data.btnImgs[0].x, this.data.btnImgs[0].y)
+        context.drawImage('../../images/icn-bg-1-40.png', this.data.btnImgs[1].x, this.data.btnImgs[1].y)
+        context.drawImage('../../images/icn-bg-2-40.png', this.data.btnImgs[2].x, this.data.btnImgs[2].y)
+        context.drawImage('../../images/icn-bg-3-40.png', this.data.btnImgs[3].x, this.data.btnImgs[3].y)
+      }
+    }
+
+    // lastly draw it!
     context.draw()
+  },
+
+  drawRoundRect (context, rect, radius, fillColor) {
+    context.beginPath();
+    context.moveTo(rect.x+radius, rect.y);
+    context.lineTo(rect.right - radius, rect.y);
+    context.arc(rect.right - radius,rect.y + radius, radius, 3*Math.PI/2,2*Math.PI, false);
+    context.lineTo(rect.right, rect.bottom - radius);
+    context.arc(rect.right - radius, rect.bottom - radius, radius, 0, Math.PI/2, false);
+    context.lineTo(rect.x + radius, rect.bottom);
+    context.arc(rect.x + radius, rect.bottom - radius, radius, Math.PI/2, Math.PI, false);
+    context.lineTo(rect.x, rect.y+radius);
+    context.arc(rect.x + radius, rect.y + radius, radius, Math.PI, 3*Math.PI/2, false);
+    context.closePath();
+    // context.setStrokeStyle(fillColor)
+    // context.stroke()
+    context.setFillStyle(fillColor)
+    context.fill()
+
+    this.drawLargeText(context, 14, '点击分享', rect.x+12, rect.y+18)
+  },
+
+  /**
+   * 要记下该按钮位置做点击判断
+   * @param  {[type]} context [description]
+   * @param  {[type]} cntrX   [description]
+   * @param  {[type]} cntrY   [description]
+   * @param  {[type]} r       [description]
+   * @param  {[type]} imgPath [description]
+   * @return {[type]}         [description]
+   */
+  drawCircleImg (context, cntrX, cntrY, r, imgPath) {
+
+    context.beginPath();
+    context.arc(cntrX, cntrY, r, 0, 2*Math.PI)
+    context.setFillStyle('#CCCCCC')
+    context.fill()
+
+    // draw image image
+    context.drawImage(imgPath, cntrX-12, cntrY-12)
+    // save the rect postion for tap detect
+    this.setData({
+      circleRect: {
+        x: cntrX - r,
+        y: cntrY - r,
+        right: cntrX + r,
+        bottom: cntrY + r
+      }
+    })
   },
 
   /**
